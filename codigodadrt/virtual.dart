43449fe2b9c642aae2970cd896090333254bf0b7
feeer-28 <darcyfernandagonzalez@gmail.com>
2024-08-14 17:33:43 -0500

    gju

---
+++ codigodadrt/virtual.dart
@@ -0,0 +1,26 @@
+import 'asignatura.dart';
+import 'plataforma.dart';
+
+class Virtual extends Asignatura {
+//Atributos
+  String? _url;
+  Plataforma? _plataforma;
+//Constructor
+  Virtual(String url, Plataforma plataforma, String nombre, int horas)
+      : super(nombre, horas) {
+    this._url = url;
+    this._plataforma = plataforma;
+//Metodos
+    String? obtenerUrl() {
+      return this._url;
+    }
+
+    Plataforma? obtenerPlataforma() {
+      return this._plataforma;
+    }
+  }
+
+  Object? obtenerUrl() {
+    return null;
+  }
+}
